a1126946b25a0c029e48400cb67489854e1ff742
SolarCoder <74742542+solar-coder@users.noreply.github.com>
2020-11-19 17:30:33 -0500

    Add files via upload

---
+++ basic-return.py
@@ -0,0 +1,7 @@
+# Author: SolarCoder
+# Instagram | @solarcoder
+
+def sum_numbers(a, b):
+    return a + b
+
+our_sum = sum_numbers(3, 5)
---
+++ return.py
@@ -0,0 +1,19 @@
+# Author: SolarCoder
+# Instagram | @solarcoder
+
+# function to calculate sum of two numbers
+
+def sum_numbers(a, b):
+    return a + b
+
+# function to square a number
+def square(c):
+    return c ** 2
+
+our_sum = sum_numbers(3, 5)
+
+squared = square(our_sum)
+
+print(squared)
+
+# Output: 64
